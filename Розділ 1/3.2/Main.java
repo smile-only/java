275c6637cf42939b15f9d03f786b35be3578bb7f
smile-only <121475262+smile-only@users.noreply.github.com>
2023-01-12 19:48:34 +0200

    Create Main.java

---
+++ Розділ 1/3.2/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Результат дорівнює: "+(2+3));
+        System.out.println("Результат дорівнює: "+(4.5*56));
+        System.out.println("Результат дорівнює: "+(2/3.0));
+    }
+}
